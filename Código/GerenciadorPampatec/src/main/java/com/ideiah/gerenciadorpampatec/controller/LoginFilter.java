b32b4cc76df3eb776e9d539b24c4fc4cfeb0b129
gutogorgen <gutogorgen@hotmail.com>
2015-09-04 15:24:26 -0300

    Atualização no login

--- Código/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/controller/LoginFilter.java
+++ Código/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/controller/LoginFilter.java
@@ -8,8 +8,6 @@ package com.ideiah.gerenciadorpampatec.controller;
 import com.ideiah.gerenciadorpampatec.model.Empreendedor;
 import java.io.IOException;
 import java.util.logging.LogRecord;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -22,7 +20,9 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author AugustoCesar
+ * @author Augusto César Görgen
+ * Funcionalidade: Criar a sessão para o empreendedor
+ * se ele não estiver logado, então é direcionado para a pagina de login.
  */
 public class LoginFilter implements Filter {
 
@@ -33,7 +33,6 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        User user = null;
         HttpSession sess = ((HttpServletRequest) request).getSession(false);
         Empreendedor user = null;
 
@@ -42,15 +41,10 @@ public class LoginFilter implements Filter {
         }
 
         if (user == null) {
-
-            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
-
             String contextPath = ((HttpServletRequest) request).getContextPath();
             ((HttpServletResponse) response).sendRedirect(contextPath + "/faces/loginEmpreendedor.xhtml");
 
         } else {
-            System.out.println("bbbbbbbbbbbbbbbbbb");
-
             chain.doFilter(request, response);
         }
     }
