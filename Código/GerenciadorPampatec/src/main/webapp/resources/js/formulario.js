a466fdcdca579b82b97503fb8d7dafdf441b8aa3
Jhonatan Moura <edisonjhonatan@gmail.com>
2015-10-27 15:32:38 -0200

    Adicionadas as estruturas para inserir comentários nos JS. Alguns já foram comentados.

--- Código/GerenciadorPampatec/src/main/webapp/resources/js/formulario.js
+++ Código/GerenciadorPampatec/src/main/webapp/resources/js/formulario.js
@@ -83,16 +83,28 @@ function exibeSextaParte() {
  * -----------------------------------------------------------------
  */
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function carregaFeedBack() {
     var feedBack = document.getElementById("formulario_cadastro_projeto:modal-feedBackSalvar");
     feedBack.setAttribute("class", "modal-feedBackSalvar modal-feedBackSalvar-in");
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function fechaFeedBack() {
     var feedBack = document.getElementById("formulario_cadastro_projeto:modal-feedBackSalvar");
     feedBack.style.display = "none";
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function  verificarCampos() {
     verificaContatos();
     verificaNegocio();
@@ -103,13 +115,20 @@ function  verificarCampos() {
 }
 
 
-
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificaContatos() {
     var empresaProjeto = document.getElementById("formulario_cadastro_projeto:empresaProjeto");
     var tabContato = document.getElementById("tabContato");
     mudarCor(empresaProjeto, tabContato, "Contato");
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificaNegocio() {
     var segmentoDeClientes = document.getElementById("formulario_cadastro_projeto:segmentoDeClientes");
     var propostaDeValor = document.getElementById("formulario_cadastro_projeto:propostaDeValor");
@@ -122,6 +141,10 @@ function verificaNegocio() {
     mudarCorLista(listaCampos, tabNegocio, "Negócio");
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificaAnaliseMercado() {
     var relacoComClientes = document.getElementById("formulario_cadastro_projeto:relacoComClientes");
     var parceriasChaves = document.getElementById("formulario_cadastro_projeto:parceriasChaves");
@@ -138,6 +161,10 @@ function verificaAnaliseMercado() {
     mudarCorLista(listaCampos, tabAnaliseMercado, "Análise de Mercado");
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificarProdutoServico() {
     var tecnologiaProcessos = document.getElementById("formulario_cadastro_projeto:tecnologiaProcessos");
     var potencialInovacaoTecnologica = document.getElementById("formulario_cadastro_projeto:potencialInovacaoTecnologica");
@@ -160,6 +187,10 @@ function verificarProdutoServico() {
     verificarPreenchimentoRadioButton();
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificaGestaoPessoas() {
     var participacaoAcionaria = document.getElementById("formulario_cadastro_projeto:participacaoAcionaria");
     var potencialEmprego = document.getElementById("formulario_cadastro_projeto:potencialEmprego");
@@ -170,6 +201,10 @@ function verificaGestaoPessoas() {
     mudarCorLista(listaCampos, tabGestaoPessoas, "Gestão de Pessoas");
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificaPlanoFinanceiro() {
     var fontesDeReceita = document.getElementById("formulario_cadastro_projeto:fontesDeReceita");
     var estruturaCustos = document.getElementById("formulario_cadastro_projeto:estruturaCustos");
@@ -188,6 +223,13 @@ function verificaPlanoFinanceiro() {
     mudarCorLista(listaCampos, tabPlanoFinanceiro, "Plano Financeiro");
 }
 
+/**
+ * @description text
+ * @param {type} listaCampos
+ * @param {type} tab
+ * @param {type} nomeCampo
+ * @returns {undefined}
+ */
 function mudarCorLista(listaCampos, tab, nomeCampo) {
     var flagCompleto = false;
     for (var i = 0; i < listaCampos.length; i++) {
@@ -208,12 +250,22 @@ function mudarCorLista(listaCampos, tab, nomeCampo) {
     }
 }
 
+/**
+ * @description text
+ */
 function verificaContatos() {
     var empresaProjeto = document.getElementById("formulario_cadastro_projeto:empresaProjeto");
     var tabContato = document.getElementById("tabContato");
     mudarCor(empresaProjeto, tabContato, "Contato");
 }
 
+/**
+ * @description text
+ * @param {type} campo
+ * @param {type} tab
+ * @param {type} nomeCampo
+ * @returns {undefined}
+ */
 function mudarCor(campo, tab, nomeCampo) {
     if (verificaPreenchimento(campo)) {
         tab.innerHTML = nomeCampo + " <i id='idIconContato' class='fa fa-check-square'></i>";
@@ -224,6 +276,11 @@ function mudarCor(campo, tab, nomeCampo) {
     }
 }
 
+/**
+ * @description text
+ * @param {type} campo
+ * @returns {Boolean}
+ */
 function verificaPreenchimento(campo) {
     if (campo.value.trim() === "") {
         return false;
@@ -232,6 +289,10 @@ function verificaPreenchimento(campo) {
     }
 }
 
+/**
+ * @description text
+ * @returns {String}
+ */
 window.onbeforeunload = function () {
     if (precisaSalvar) {
         return "Você têm alterações que ainda não foram salvas.Têm certeza que quer sair da página?";
@@ -239,11 +300,18 @@ window.onbeforeunload = function () {
 };
 
 //trim completo
+/**
+ * @description text
+ * @returns {String.prototype@call;replace}
+ */
 String.prototype.trim = function () {
     return this.replace(/^\s+|\s+$/g, "");
 };
 
-
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function verificarPreenchimentoRadioButton() {
     var elementos = document.getElementsByClassName('ui-radiobutton-box ui-widget ui-corner-all ui-state-default ui-state-active');
     for (var i = 0; i < elementos.length; i++) {
@@ -251,6 +319,13 @@ function verificarPreenchimentoRadioButton() {
     }
 }
 
+/**
+ * @description text
+ * @param {type} listaComponentes
+ * @param {type} pai
+ * @param {type} contador
+ * @returns {undefined}
+ */
 function percorrerArvoreObejetos(listaComponentes, pai, contador) {
     for (var i = 0; i < pai.children.length; i++) {
         if (pai.children[i].tagName === listaComponentes[contador]) {
@@ -268,11 +343,18 @@ function percorrerArvoreObejetos(listaComponentes, pai, contador) {
 
 
 
-
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function infoSalvar() {
     $("#modalInfoSalvar").modal();
 }
 
+/**
+ * @description text
+ * @returns {undefined}
+ */
 function confirmacaoDeEnvio() {
     $("#modalInfoDeEnvio").modal();
 }
@@ -418,8 +500,10 @@ function carregaPagina() {
     }
 }
 
+
 /**
- * Metodo que exibe o menu vertical da etapa elaboração
+ * @description Função que exibe o menu vertical referente a etapa de elaboração
+ * @returns {undefined}
  */
 function mostra_vertical_elaboracao() {
 
@@ -440,7 +524,8 @@ function mostra_vertical_elaboracao() {
 
 
 /**
- * Metodo que exibe o menu vertical da etapa Pré-Avalição
+ * @description Função que exibe o menu vertical referente a etapa de pré avaliação
+ * @returns {undefined}
  */
 function mostra_vertical_pre_avaliacao() {
 
@@ -457,6 +542,105 @@ function mostra_vertical_pre_avaliacao() {
     divIncubacao.setAttribute("class", "esconder-div");
 
 
+}
+
+
+
+/**
+ * @description Função que exibe o menu vertical referente a etapa de Avaliação
+ * @returns {undefined}
+ */
+function mostra_avaliacao() {
+
+    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
+    divElaboracao.setAttribute("class", "esconder-div");
+    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
+    divPreAvaliacao.setAttribute("class", "esconder-div");
+    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
+    divAvaliacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
+    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
+    divFormalizacao.setAttribute("class", "esconder-div");
+    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
+    divIncubacao.setAttribute("class", "esconder-div");
+
+}
+
+/**
+ * @description Função que exibe o menu vertical referente a etapa de Formalização
+ * @returns {undefined}
+ */
+function mostra_formalizacao() {
+
+    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
+    divElaboracao.setAttribute("class", "esconder-div");
+    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
+    divPreAvaliacao.setAttribute("class", "esconder-div");
+    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
+    divAvaliacao.setAttribute("class", "esconder-div");
+    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
+    divFormalizacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
+    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
+    divIncubacao.setAttribute("class", "esconder-div");
+
+
+}
+
+/**
+ * @description Função que exibe o menu vertical referente a etapa de Incubação
+ * @returns {undefined}
+ */
+function mostra_incubacao() {
+
+    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
+    divElaboracao.setAttribute("class", "esconder-div");
+    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
+    divPreAvaliacao.setAttribute("class", "esconder-div");
+    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
+    divAvaliacao.setAttribute("class", "esconder-div");
+    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
+    divFormalizacao.setAttribute("class", "esconder-div");
+    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
+    divIncubacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
+
+}
+
+/**
+ * @description Função que bloqueia todos os campos da tela Enviar Projeto. Para o empreendedor apenas poder visualizar e não editar.
+ * @returns {undefined}
+ */
+function bloquearCampos() {
+    var d = document.getElementById('myTabContent').getElementsByTagName('input');
+    var botaoOutro = document.getElementById('formulario_cadastro_projeto:estagioDeEvolucao');
+    botaoOutro.disabled = "true";
+
+    for (var i = 0; i < d.length; i++) {
+//        alert(d[i].value);
+        d[i].disabled = "true";
+    }
+    var d2 = document.getElementById('myTabContent').getElementsByTagName('textarea');
+    for (var i = 0; i < d2.length; i++) {
+//        alert(d[i].value);
+        d2[i].disabled = "true";
+    }
+}
+
+
+/**
+ * @description Função para alternar entre as DIVs
+ * @param {type} referencia
+ * @returns {undefined}
+ */
+function mostraDIV(referencia) {
+//    div1 = document.querySelector(".classeConteudo");
+    div1 = document.getElementsByClassName("classeConteudo");
+    for (var i = 0; i < div1.length; i++) {
+
+        if (div1[i].getAttribute("id") === referencia) {
+            div1[i].setAttribute("class", "classeConteudo col-md-10");
+        } else {
+            div1[i].setAttribute("class", "classeConteudo esconder-div");
+        }
+    }
 }
 
 /**
@@ -480,83 +664,4 @@ function addFoco(idDoItem) {
  */
 function retornarCorOriginal() {
     objetoAnterior.setAttribute("class", classeAnterior);
-}
-
-function mostra_avaliacao() {
-
-    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
-    divElaboracao.setAttribute("class", "esconder-div");
-    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
-    divPreAvaliacao.setAttribute("class", "esconder-div");
-    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
-    divAvaliacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
-    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
-    divFormalizacao.setAttribute("class", "esconder-div");
-    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
-    divIncubacao.setAttribute("class", "esconder-div");
-
-}
-function mostra_formalizacao() {
-
-    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
-    divElaboracao.setAttribute("class", "esconder-div");
-    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
-    divPreAvaliacao.setAttribute("class", "esconder-div");
-    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
-    divAvaliacao.setAttribute("class", "esconder-div");
-    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
-    divFormalizacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
-    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
-    divIncubacao.setAttribute("class", "esconder-div");
-
-
-}
-function mostra_incubacao() {
-
-    var divElaboracao = document.getElementById("vertical_etapa_elaboracao");
-    divElaboracao.setAttribute("class", "esconder-div");
-    var divPreAvaliacao = document.getElementById("vertical_etapa_pre_avaliacao");
-    divPreAvaliacao.setAttribute("class", "esconder-div");
-    var divAvaliacao = document.getElementById("vertical_etapa_avaliacao");
-    divAvaliacao.setAttribute("class", "esconder-div");
-    var divFormalizacao = document.getElementById("vertical_etapa_formalizacao");
-    divFormalizacao.setAttribute("class", "esconder-div");
-    var divIncubacao = document.getElementById("vertical_etapa_incubacao");
-    divIncubacao.setAttribute("class", "col-md-2 text-center bounceInLeft animated");
-
-}
-
-/**
- * Função que bloqueia todos os campos da tela EnviarProjeto. Para o Empreendedor apenas poder visualizar e não editar.
- */
-function bloquearCampos() {
-    var d = document.getElementById('myTabContent').getElementsByTagName('input');
-    var botaoOutro = document.getElementById('formulario_cadastro_projeto:estagioDeEvolucao');
-    botaoOutro.disabled = "true";
-
-    for (var i = 0; i < d.length; i++) {
-//        alert(d[i].value);
-        d[i].disabled = "true";
-    }
-    var d2 = document.getElementById('myTabContent').getElementsByTagName('textarea');
-    for (var i = 0; i < d2.length; i++) {
-//        alert(d[i].value);
-        d2[i].disabled = "true";
-    }
-}
-
-/**
- * Função para alternar entre as DIVS
- */
-function mostraDIV(referencia) {
-//    div1 = document.querySelector(".classeConteudo");
-    div1 = document.getElementsByClassName("classeConteudo");
-    for (var i = 0; i < div1.length; i++) {
-
-        if (div1[i].getAttribute("id") === referencia) {
-            div1[i].setAttribute("class", "classeConteudo col-md-10");
-        } else {
-            div1[i].setAttribute("class", "classeConteudo esconder-div");
-        }
-    }
 }
