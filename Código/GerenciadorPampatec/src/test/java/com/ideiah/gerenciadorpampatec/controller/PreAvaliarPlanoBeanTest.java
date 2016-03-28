3d8437c8307cb5aad54bd498062286a55f5eebdb
Juliano M <j.rodovalho.m@gmail.com>
2016-03-28 16:50:22 -0300

    Bean Test - Ignorando a classe teste.

--- Código/GerenciadorPampatec/src/test/java/com/ideiah/gerenciadorpampatec/controller/PreAvaliarPlanoBeanTest.java
+++ Código/GerenciadorPampatec/src/test/java/com/ideiah/gerenciadorpampatec/controller/PreAvaliarPlanoBeanTest.java
@@ -14,11 +14,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
  * @author Claudio
  */
+@Ignore
 public class PreAvaliarPlanoBeanTest {
 
     PreAvaliarPlanoBean instance;
