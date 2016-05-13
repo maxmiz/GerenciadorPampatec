d89e75f4024929e7c51a201c248ebee03cb8ca2e
Vinícius Lopes <vinicius_silva_lopes@hotmail.com>
2016-05-13 12:00:54 -0300

    Interface - Adicção do tratamento de projetos REVISANDO no workflow e adicionando feedback no Excluir da tabela custos variaveis

--- Código/GerenciadorPampatec/src/main/webapp/resources/js/formulario.js
+++ Código/GerenciadorPampatec/src/main/webapp/resources/js/formulario.js
@@ -574,6 +574,30 @@ function carregaPagina() {
 
             break;
             
+//      REVISANDO = 13;
+        case 13:
+            etapa3.innerHTML = "<b>Avaliação</b>";
+            etapa4.innerHTML = "<b>Formalização</b>";
+            etapa5.innerHTML = "<b>Incubação</b>";
+            etapa1.setAttribute("class", "active, etapaSelecionada");
+            etapa2.setAttribute("class", "active, etapaAtual");
+            mostra_vertical_elaboracao();
+
+            etapaAtualDoWorkflow = "etapa2";
+
+            mostraDIV('div_revisar_plano');
+            
+            addFocoBotao("botao_revisar");
+
+            var etapa = document.getElementById("etapa3");
+            etapa.setAttribute("style", "cursor: default;");
+            var etapa = document.getElementById("etapa4");
+            etapa.setAttribute("style", "cursor: default;");
+            var etapa = document.getElementById("etapa5");
+            etapa.setAttribute("style", "cursor: default;");
+
+            break;
+        
 //      RESUBMETIDO = 11;
         case 11:
             etapa3.innerHTML = "<b>Avaliação</b>";
