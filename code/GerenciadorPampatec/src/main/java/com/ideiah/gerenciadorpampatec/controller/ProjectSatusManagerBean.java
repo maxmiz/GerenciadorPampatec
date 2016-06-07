45df358fe28e2b803af75a84b6eb31594950b9a9
Juliano M <j.rodovalho.m@gmail.com>
2016-06-07 16:38:04 -0300

    Bean - Iniciando a construção de uma classe que trabelhe os status do projeto.

---
+++ code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/controller/ProjectSatusManagerBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ideiah.gerenciadorpampatec.controller;
+
+import com.ideiah.gerenciadorpampatec.model.GerenteRelacionamento;
+import com.ideiah.gerenciadorpampatec.model.Projeto;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * Classe para gerenciar o status do projeto em todo o sistema.</p>
+ *
+ * @author unipampa
+ */
+public class ProjectSatusManagerBean {
+
+    public ProjectSatusManagerBean() {
+    }
+
+    /**
+     * <p>
+     * Muda o status do projeto para em pré-avaliação quando a sessão do gerente
+     * é finalizada.
+     * </p>
+     */
+    protected void tratamentoSessaoSendoAvaliado() {
+        System.out.println("Entou no PSMB...");
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+        GerenteRelacionamento gerente = (GerenteRelacionamento) session.getAttribute("gerente");
+        Projeto projeto = (Projeto) session.getAttribute("projetoSelecionado");
+
+        if (gerente != null && projeto != null && projeto.getStatus() == Projeto.SENDO_AVALIADO) {
+            projeto.setStatus(Projeto.EM_PRE_AVALIACAO);
+            session.removeAttribute("projetoSelecionado");
+        }
+    }
+}
