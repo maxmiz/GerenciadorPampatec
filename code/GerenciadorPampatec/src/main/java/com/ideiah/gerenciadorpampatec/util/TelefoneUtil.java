cd0144a3acf8ba0c6a39bdaf05fdefd4a057d107
Juliano M <j.rodovalho.m@gmail.com>
2016-06-28 15:00:01 -0300

    Util - Melhoria no javadoc da classe para melhor explicação da mesma.

--- code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/TelefoneUtil.java
+++ code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/TelefoneUtil.java
@@ -1,13 +1,30 @@
 package com.ideiah.gerenciadorpampatec.util;
 
+/**
+ * <p>Classe responsável por validar números de telefone.</p>
+ * 
+ * @author unipampa
+ */
 public class TelefoneUtil {
 
+    public TelefoneUtil() {
+    }
+
+    /**
+     * <p>
+     * Método para remover os parênteses da <code>String</code> telefone.</p>
+     *
+     * @param telefone
+     * @return
+     */
     public static String removeParentesesTelefone(String telefone) {
         String numero = "";
-        for (int i = 0; i < telefone.length(); i++) {
-           if(Character.isDigit(telefone.charAt( i ))){
-          numero = numero.concat(String.valueOf(telefone.charAt( i )));
-           }  
+        if (telefone != null) {
+            for (int i = 0; i < telefone.length(); i++) {
+                if (Character.isDigit(telefone.charAt(i))) {
+                    numero = numero.concat(String.valueOf(telefone.charAt(i)));
+                }
+            }
         }
         return numero;
     }
