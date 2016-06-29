21228404501e7489b7852234b06661b45c97ff96
Juliano M <j.rodovalho.m@gmail.com>
2016-06-29 17:44:14 -0300

    Test - Início da implementação da classe de teste unitário para a classe ComparadorEnvioUtil.

---
+++ code/GerenciadorPampatec/src/test/java/com/ideiah/gerenciadorpampatec/util/ComparadorEnvioUtilTest.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ideiah.gerenciadorpampatec.util;
+
+import com.ideiah.gerenciadorpampatec.model.Projeto;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author unipampa
+ */
+public class ComparadorEnvioUtilTest {
+    
+    public ComparadorEnvioUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testConstrutorComparadorEnvioUtil(){
+        System.out.println("testConstrutorComparadorEnvioUtil");
+        ComparadorEnvioUtil expResult = new ComparadorEnvioUtil();
+        assertNotNull(expResult);
+    }
+    
+    
+    /**
+     * Test of compare method, of class ComparadorEnvioUtil.
+     */
+    @Test
+    public void testCompare() {
+        System.out.println("compare_Null");
+        Projeto o1 = null;
+        Projeto o2 = null;
+        ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
+        int expResult = 0;
+        int result = instance.compare(o1, o2);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of compare method, of class ComparadorEnvioUtil.
+     */
+    @Test
+    public void testCompare2() {
+        System.out.println("compare");
+        
+        Projeto o1 = new Projeto();
+        Projeto o2 = new Projeto();
+        
+        Date date = new Date();
+        o1.setDataEnvio(date);
+        o2.setDataEnvio(date);
+        
+        ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
+        int expResult = 0;
+        int result = instance.compare(o1, o2);
+        assertEquals(expResult, result);
+    }
+    
+    
+    /**
+     * Test of compare method, of class ComparadorEnvioUtil.
+     */
+    @Test
+    public void testCompare3() {
+        System.out.println("compare");
+        
+        Projeto o1 = new Projeto();
+        Projeto o2 = new Projeto();
+        
+        Date date = new Date();
+        o1.setDataEnvio(date);
+        o2.setDataEnvio(null);
+        
+        ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
+        int expResult = 0;
+        int result = instance.compare(o1, o2);
+        assertEquals(expResult, result);
+    }
+    
+}
