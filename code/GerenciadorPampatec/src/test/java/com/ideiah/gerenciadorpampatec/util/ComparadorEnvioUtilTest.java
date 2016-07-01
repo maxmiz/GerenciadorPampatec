7c3c7a189747f2cf2acc69f87f80dcfbea7d3061
Juliano M <j.rodovalho.m@gmail.com>
2016-07-01 09:51:52 -0300

    Test - Finalização dos testes unitários da classe ComparadorEnvioUtil.

--- code/GerenciadorPampatec/src/test/java/com/ideiah/gerenciadorpampatec/util/ComparadorEnvioUtilTest.java
+++ code/GerenciadorPampatec/src/test/java/com/ideiah/gerenciadorpampatec/util/ComparadorEnvioUtilTest.java
@@ -6,104 +6,87 @@
 package com.ideiah.gerenciadorpampatec.util;
 
 import com.ideiah.gerenciadorpampatec.model.Projeto;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 /**
+ * <p>
+ * Classe para realizar testes unitários na classe
+ * <code>ComparadorEnvioUtil</code>.</p>
  *
  * @author unipampa
  */
 public class ComparadorEnvioUtilTest {
-    
+
     public ComparadorEnvioUtilTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
-     * 
+     * Test of compare method, of class ComparadorEnvioUtil.
+     * <p>
+     * Teste para validar datas iguais.</p>
      */
     @Test
-    public void testConstrutorComparadorEnvioUtil(){
-        System.out.println("testConstrutorComparadorEnvioUtil");
-        ComparadorEnvioUtil expResult = new ComparadorEnvioUtil();
-        assertNotNull(expResult);
-    }
-    
-    
-    /**
-     * Test of compare method, of class ComparadorEnvioUtil.
-     */
-    @Test(expected = NullPointerException.class)
     public void testCompare() {
-        System.out.println("compare_Null");
-        Projeto o1 = null;
-        Projeto o2 = null;
-        ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
-        int expResult = 0;
-        int result = instance.compare(o1, o2);
-        assertEquals(expResult, result);
-    }
-    
-    /**
-     * Test of compare method, of class ComparadorEnvioUtil.
-     */
-    @Test
-    public void testCompare2() {
-        System.out.println("compare");
-        
+        System.out.println("Compare_EqualsDate");
+
         Projeto o1 = new Projeto();
         Projeto o2 = new Projeto();
-        
+
         Date date = new Date();
         o1.setDataEnvio(date);
         o2.setDataEnvio(date);
-        
+
         ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
         int expResult = 0;
         int result = instance.compare(o1, o2);
         assertEquals(expResult, result);
     }
-    
-    
+
     /**
      * Test of compare method, of class ComparadorEnvioUtil.
+     * <p>
+     * Teste para validar data diferentes.</p>
      */
-    @Test(expected = NullPointerException.class)
-    public void testCompare3() {
-        System.out.println("compare");
-        
+    @Test
+    public void testCompare2() {
+        System.out.println("Compare_NotSame");
+
         Projeto o1 = new Projeto();
         Projeto o2 = new Projeto();
-        
+
         Date date = new Date();
+        GregorianCalendar date2 = new GregorianCalendar(
+                2015, Calendar.FEBRUARY, 12);
         o1.setDataEnvio(date);
-        o2.setDataEnvio(null);
-        
+        o2.setDataEnvio(date2.getGregorianChange());
+
         ComparadorEnvioUtil instance = new ComparadorEnvioUtil();
         int expResult = 0;
         int result = instance.compare(o1, o2);
-        assertEquals(expResult, result);
+        assertNotSame(expResult, result);
     }
-    
 }
