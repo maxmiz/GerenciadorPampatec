aef71e7cf78a6329d409e891b05b33d7deaad900
Vinícius Lopes <vinicius_silva_lopes@hotmail.com>
2016-07-05 14:51:14 -0300

    Util - Iclusao do corpo do email para resubmissao (após melhoria)

--- code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
+++ code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
@@ -263,6 +263,82 @@ public class EmailUtil {
             }
         }
     }
+    /**
+     * <P>
+     * Método responsável por enviar email à todos os GERENTES quando um novo projeto
+     * e submitido para a Pré-Avaliação.</P>
+     *
+     * @param listaDeGerentes
+     * @param projetoNome
+     */
+    public synchronized static void mandarEmailRessubmissao(String projetoNome, ArrayList<GerenteRelacionamento> listaDeGerentes) {
+        
+        if (projetoNome != null && listaDeGerentes != null) {
+            if (!projetoNome.isEmpty() && !listaDeGerentes.isEmpty()) {
+                
+                for (int i = 0; i < listaDeGerentes.size(); i++) {
+
+                    emailHtml = new HtmlEmail();
+                    try {
+                        emailHtml.setHostName("smtp.googlemail.com");
+                        emailHtml.setSmtpPort(465);
+                        emailHtml.setAuthenticator(
+                                new DefaultAuthenticator(getAuthEmail(), getAuthPassphrase())
+                        );
+                        emailHtml.setSSLOnConnect(true);
+                        emailHtml.addTo(listaDeGerentes.get(i).getEmail());
+                        emailHtml.setFrom(getAuthEmail(), "Ideiah Developer");
+                        emailHtml.setSubject("PampaTec - Gerenciador de Projetos");
+                        
+                        StringBuffer msg = new StringBuffer();
+
+                        // <editor-fold defaultstate="collapsed" desc="Mensagem do Email"> 
+                        msg.append("<!DOCTYPE html>\n"
+                                + "<html lang=\"pt-br\">\n"
+                                + "	<head>\n"
+                                + "		<meta charset=\"utf-8\"/>\n"
+                                + "		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n"
+                                + "    </head>\n"
+                                + "    <body style=\"margin: 0;padding-top: 10px;\">                    \n"
+                                + "		<div style=\"width: 15%;\"></div>\n"
+                                + "		<div style=\"text-align: center; width: 70%; min-height: 20px; padding: 19px; margin-bottom: 20px; background-color: #f5f5f5; border: 2px solid #3FB618; border-radius: 0; -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05); box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);\">\n"
+                                + "			<h1>Plano de Negócio em Melhoria Renviado!</h1>\n"
+                                + "			<form>\n"
+                                + "				<div style=\"margin-bottom: 15px;\">\n"
+                                + "					<a>Olá <strong>" + listaDeGerentes.get(i).getNome() + "</strong>! Notificamos que Plano de Negócio chamado <strong><i>" + projetoNome + "</i></strong> foi renviado.\n"
+                                + "							<br/>\n"
+                                + "							<br/>Para visualizar o plano de negócio melhorado, clique na opção abaixo.</a>\n"
+                                + "							<br/>\n"
+                                + "							<br/>\n"
+                                + "							<a style=\"display: inline-block; margin-bottom: 0; font-weight: normal; text-align: center; vertical-align: middle; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; background-image: none; border: 1px solid transparent; white-space: nowrap; padding: 10px 18px; font-size: 15px; line-height: 1.42857143; border-radius: 0; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; color: #ffffff; background-color: #3fb618; border-color: #3fb618;\" href=\"http://localhost:8084/GerenciadorPampatec/faces/terminarCadastroEmpreendedor.xhtml\">\n"
+                                + "								<strong>Acessar o sistema</strong>\n"
+                                + "							</a>\n"
+                                + "				</div>\n"
+                                + "			</form>\n"
+                                + "			<div style=\"padding: 10px; overflow: auto; border-top: 1px solid #e5e5e5;\">\n"
+                                + "				<img src=\"http://i.imgur.com/4c1IDDR.jpg\" alt=\"Logotipo da Unipampa\" style=\"width: 100px;height: 65px;float: left;\"/>\n"
+                                + "				\n"
+                                + "				<img src=\"http://i.imgur.com/g9hBPAV.jpg\" alt=\"Logotipo do PampaTec\" style=\"width: 100px;height: 65px;float: right;\"/> \n"
+                                + "			</div>\n"
+                                + "		</div>\n"
+                                + "		<div style=\"width: 15%;\"></div>\n"
+                                + "		<!-- CSS embutido in line -->               \n"
+                                + "    </body>\n"
+                                + "</html>");
+                        // </editor-fold>
+
+                        emailHtml.setHtmlMsg(msg.toString());
+                        
+                        emailHtml.send();
+                        
+                    } catch (EmailException ex) {
+                        Logger.getLogger(EmailUtil.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+            }
+        }
+    }
 
     /**
      * <p>
