793f79a9dde23a0242bb8951c619dd32e02b5cda
Maicon Alves <alvesmaiconn@gmail.com>
2016-08-05 11:44:42 -0300

    Util - criado email para confirmação de email

--- code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
+++ code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
@@ -338,6 +338,75 @@ public class EmailUtil {
             }
         }
     }
+    
+    
+    /**
+     * <p>
+     * Envio de email para confirmacao de email.</p>
+     *
+     * @param empreendedorNome
+     * @param email
+     * @param idUnico
+     */
+    public static void mandarEmailConfirmacao(String empreendedorNome,
+            String email, String idUnico) {
+        if (empreendedorNome != null && email != null && idUnico != null) {
+            if (!empreendedorNome.isEmpty() && !email.isEmpty() && !idUnico.isEmpty()) {
+
+                emailHtml = new HtmlEmail();
+                try {
+                    emailHtml.setHostName("smtp.googlemail.com");
+                    emailHtml.setSmtpPort(465);
+                    emailHtml.setAuthenticator(
+                            new DefaultAuthenticator(getAuthEmail(), getAuthPassphrase())
+                    );
+                    emailHtml.setSSLOnConnect(true);
+                    emailHtml.addTo(email);
+                    emailHtml.setFrom(getAuthEmail(), "Ideiah Developer");
+                    emailHtml.setSubject("PampaTec - Gerenciador de Projetos");
+
+                    StringBuffer msg = new StringBuffer();
+
+                    // <editor-fold defaultstate="collapsed" desc="Mensagem do Email">
+                    msg.append("<html lang=\"pt-br\">\n"
+                            + "  <head>\n"
+                            + "    <meta charset=\"utf-8\"/>\n"
+                            + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n"
+                            + "  </head>\n"
+                            + "  <body background=\"resources/imagens/21.jpg\" style=\"margin: 0;padding-top: 10px\">\n"
+                            + "\n"
+                            + "        <div class=\"col-md-4\" style=\"width: 66.333333%\"/>\n"
+                            + "        <div class=\"col-md-4 text-center well\" style=\"text-align: center;width: 66.333333%;min-height: 20px;padding: 19px;margin-bottom: 20px;background-color: #f5f5f5;border: 2px solid #3FB618;border-radius: 0;-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05)\">\n"
+                            + "            <h1 class=\"fonteBranca\">Confirmar email</h1>\n"
+                            + "            <form class=\"form-horizontal\">\n"
+                            + "                    <div class=\"form-group\" style=\"margin-bottom: 15px\">\n"
+                            + "                        <a>Olá, " + empreendedorNome + "!</a><br/><br/>"
+                            + "                        Você se cadastrou no Sistema de gerenciamento de projetos do PampaTec, mas nós ainda precisamos verificar seu e-mail.\n"
+                            + "                        <br/><br/>Para confirmar o seu email clique na opção abaixo.</a><br/><br/><a class=\"btn btn-success\" href=\"http://localhost:8080/GerenciadorPampatec/confirmarEmail.jsf?id=" + idUnico + " \"  style=\"color: #fff;background-color: #3fb618;border-color: #3fb618;display: inline-block;margin-bottom: 0;font-weight: normal;text-align: center;vertical-align: middle;-ms-touch-action: manipulation;touch-action: manipulation;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 10px 18px;font-size: 15px;line-height: 1.428571;border-radius: 0;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none\"><strong>Confirmar o email</strong></a>\n"
+                            + "                    </div>\n"
+                            + "                     <div class=\"footer\" style=\"padding: 20px;text-align: center;border-top: 1px solid #e5e5e5\">\n"
+                            + "                         <img src=\"http://i.imgur.com/4c1IDDR.jpg\" alt=\"Logotipo da Unipampa\" style=\"width:80px;height:45px;\"/>\n"
+                            + "                         <img src=\"http://i.imgur.com/g9hBPAV.jpg\" alt=\"Logotipo do PampaTec\" style=\"width:80px;height:45px;\">\n"
+                            + "                      </div>\n"
+                            + "        </form></div>\n"
+                            + "        <div class=\"col-md-4\" style=\"width: 66.333333%\"/>\n"
+                            + "\n"
+                            + "        <!-- CSS embutido in line -->\n"
+                            + "\n"
+                            + "        </body>\n"
+                            + "</html>");
+                    // </editor-fold>
+
+                    emailHtml.setHtmlMsg(msg.toString());
+
+                    emailHtml.send();
+
+                } catch (EmailException ex) {
+                    Logger.getLogger(EmailUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
 
     /**
      * <p>
