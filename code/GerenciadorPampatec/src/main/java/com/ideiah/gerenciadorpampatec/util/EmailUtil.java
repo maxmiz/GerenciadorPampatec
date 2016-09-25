a5b309a7126b0f32af66ffc116ed7fb412dc8415
Peterson Rodrigues <petersonlrrodrigues@gmail.com>
2016-09-24 23:47:34 -0300

    Adição de um servidor de e-mails, além de alteração de onde chama o método antigo de confirmação de email para o novo.

--- code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
+++ code/GerenciadorPampatec/src/main/java/com/ideiah/gerenciadorpampatec/util/EmailUtil.java
@@ -16,6 +16,12 @@ import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
 import org.apache.commons.mail.MultiPartEmail;
 import org.apache.commons.mail.SimpleEmail;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import javax.ws.rs.core.MediaType;
 
 /**
  * <p>
@@ -168,7 +174,7 @@ public class EmailUtil {
                                 + "			<h1>Plano de negócio disponível para avaliação!</h1>\n"
                                 + "			<form>\n"
                                 + "				<div style=\"margin-bottom: 15px;\">\n"
-                                + "					<a>Olá <strong>" + listaDeGerentes.get(i).getNome() + "</strong>! Notificamos que Plano de Negócio chamado <strong>" + projetoNome + "</strong>, do empreendedor <strong>"+ empreendedorNome +"</strong>, <br/>atualizou seu status para <strong>" + statusProjeto + "</strong> em <strong>"+ submissaoData +"</strong> às <strong>"+ submissaoHora +"</strong>.\n"
+                                + "					<a>Olá <strong>" + listaDeGerentes.get(i).getNome() + "</strong>! Notificamos que Plano de Negócio chamado <strong>" + projetoNome + "</strong>, do empreendedor <strong>" + empreendedorNome + "</strong>, <br/>atualizou seu status para <strong>" + statusProjeto + "</strong> em <strong>" + submissaoData + "</strong> às <strong>" + submissaoHora + "</strong>.\n"
                                 + "							<br/>\n"
                                 + "							<br/>Para acessar o sistema Gerenciador Pampatec, clique na opção abaixo.</a>\n"
                                 + "							<br/>\n"
@@ -252,7 +258,7 @@ public class EmailUtil {
                             + "							<br/>Por favor, <strong>clique na opção a baixo</strong> para redefinir uma nova senha e continuar.</a>\n"
                             + "							<br/>\n"
                             + "							<br/>\n"
-                            + "							<a style=\"display: inline-block; margin-bottom: 0; font-weight: normal; text-align: center; vertical-align: middle; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; background-image: none; border: 1px solid transparent; white-space: nowrap; padding: 10px 18px; font-size: 15px; line-height: 1.42857143; border-radius: 0; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; color: #ffffff; background-color: #3fb618; border-color: #3fb618;\" href=\"" + RedirectManager.getBaseURL() + "recuperarSenha.jsf?id=" + idUnico +  "\">"
+                            + "							<a style=\"display: inline-block; margin-bottom: 0; font-weight: normal; text-align: center; vertical-align: middle; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; background-image: none; border: 1px solid transparent; white-space: nowrap; padding: 10px 18px; font-size: 15px; line-height: 1.42857143; border-radius: 0; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; color: #ffffff; background-color: #3fb618; border-color: #3fb618;\" href=\"" + RedirectManager.getBaseURL() + "recuperarSenha.jsf?id=" + idUnico + "\">"
                             + "								<strong>Redefinir minha senha</strong>\n"
                             + "							</a>\n"
                             + "				</div>\n"
@@ -432,7 +438,81 @@ public class EmailUtil {
         }
         return true;
     }
-    
+
+    /**
+     * <p>
+     * Envio de email para confirmacao de email.</p>
+     *
+     * @param empreendedorNome
+     * @param email
+     * @param idUnico
+     * @return
+     */
+    public synchronized static boolean mandarEmailConfirmacaoNew(String empreendedorNome,
+            String email, String idUnico) {
+
+        if (empreendedorNome != null && email != null && idUnico != null) {
+            if (!empreendedorNome.isEmpty() && !email.isEmpty() && !idUnico.isEmpty()) {
+                StringBuffer msg = new StringBuffer();
+
+                // <editor-fold defaultstate="collapsed" desc="Mensagem do Email">
+                msg.append("<!DOCTYPE html>\n"
+                        + "<html lang=\"pt-br\">\n"
+                        + "	<head>\n"
+                        + "		<meta charset=\"utf-8\"/>\n"
+                        + "		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n"
+                        + "    </head>\n"
+                        + "    <body style=\"margin: 0;padding-top: 10px;\">                    \n"
+                        + "		<div style=\"width: 15%;\"></div>\n"
+                        + "		<div style=\"text-align: center; width: 70%; min-height: 20px; padding: 19px; margin-bottom: 20px; background-color: #f5f5f5; border: 2px solid #3FB618; border-radius: 0; -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05); box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);\">\n"
+                        + "			<h1>Confirmar email</h1>\n"
+                        + "			<form>\n"
+                        + "				<div style=\"margin-bottom: 15px;\">\n"
+                        + "					<a>\n"
+                        + "                            <br/>Olá, " + empreendedorNome + "!</a><br/>\n"
+                        + "                            Você se cadastrou no Sistema de gerenciamento de projetos do PampaTec, mas nós ainda precisamos verificar seu e-mail.\n"
+                        + "                            <br/><br/>Para confirmar o seu email clique na opção abaixo.</a>\n"
+                        + "							<br/>\n"
+                        + "							<br/>\n"
+                        + "							<a style=\"display: inline-block; margin-bottom: 0; font-weight: normal; text-align: center; vertical-align: middle; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; background-image: none; border: 1px solid transparent; white-space: nowrap; padding: 10px 18px; font-size: 15px; line-height: 1.42857143; border-radius: 0; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; color: #ffffff; background-color: #3fb618; border-color: #3fb618;\" href=\"" + RedirectManager.getBaseURL() + "verificarEmail.jsf?id=" + idUnico + " \">\n"
+                        + "								<strong>Confirmar meu e-mail</strong>\n"
+                        + "							</a>\n"
+                        + "				</div>\n"
+                        + "			</form>\n"
+                        + "			<div style=\"padding: 10px; overflow: auto; border-top: 1px solid #e5e5e5;\">\n"
+                        + "				<img src=\"http://i.imgur.com/4c1IDDR.jpg\" alt=\"Logotipo da Unipampa\" style=\"width: 100px;height: 65px;float: left;\"/>\n"
+                        + "				\n"
+                        + "				<img src=\"http://i.imgur.com/g9hBPAV.jpg\" alt=\"Logotipo do PampaTec\" style=\"width: 100px;height: 65px;float: right;\"/> \n"
+                        + "			</div>\n"
+                        + "		</div>\n"
+                        + "		<div style=\"width: 15%;\"></div>\n"
+                        + "		<!-- CSS embutido in line -->               \n"
+                        + "    </body>\n"
+                        + "</html>");
+                // </editor-fold>
+
+                SendSimpleMessage(email, msg.toString(), "Confirmação E-mail");
+            }
+        }
+        return true;
+    }
+
+    public static ClientResponse SendSimpleMessage(String email, String msg, String assunto) {
+        Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("api",
+                "key-18a6549e405b5fbd9e5b8370075c039b"));
+        com.sun.jersey.api.client.WebResource webResource
+                = client.resource("https://api.mailgun.net/v3/ideiah.com"
+                        + "/messages");
+        MultivaluedMapImpl formData = new MultivaluedMapImpl();
+        formData.add("from", "Gerenciador Pampatec <naoresponder@ideiah.com>");
+        formData.add("to", email);
+        formData.add("subject", "Gerenciador Pampatec " + assunto);
+        formData.add("html", msg);
+        return webResource.type(MediaType.APPLICATION_FORM_URLENCODED).
+                post(ClientResponse.class, formData);
+    }
+
     /**
      * <p>
      * Envio de email para confirmacao de email.</p>
